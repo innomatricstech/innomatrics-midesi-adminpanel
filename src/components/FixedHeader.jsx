b8e4a49e780a85a513983a7f2d338e0398374cac
innomatricstech <innomatricstech@gmail.com>
2025-11-06 18:53:17 +0530

    Update FixedHeader.jsx

--- src/components/FixedHeader.jsx
+++ src/components/FixedHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/fixedHeader.css";
+import "../styles/fixedheader.css";
 import { db, collection, getDocs } from "../firebase";
 
 const FixedHeader = ({ onSearchChange }) => {
